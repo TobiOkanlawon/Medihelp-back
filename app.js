4be61104769152d2681adcbfe2153c0d7364b2ff
Tobi Okanlawon <tobiinlondon34@gmail.com>
2021-07-28 00:14:25 +0100

    Add process.env call

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ const server = new ApolloServer({
 const app = express();
 server.applyMiddleware({app})
 
-app.listen({port: 4000}, ()=>{
+app.listen({port: process.env.PORT || 4000}, ()=>{
     console.log(`Server is running!${server.graphqlPath}`)
 })
